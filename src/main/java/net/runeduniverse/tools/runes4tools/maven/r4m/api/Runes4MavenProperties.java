befba59787770ad6029e7d08f77a35fdf1122743
Pl4yingNight <pl4yingnight@gmail.com>
2022-12-10 15:56:35 +0100

    patched r4m properties

--- src/main/java/net/runeduniverse/tools/runes4tools/maven/r4m/api/Runes4MavenProperties.java
+++ src/main/java/net/runeduniverse/tools/runes4tools/maven/r4m/api/Runes4MavenProperties.java
@@ -2,7 +2,7 @@ package net.runeduniverse.tools.runes4tools.maven.r4m.api;
 
 public interface Runes4MavenProperties {
 	public static String GROUP_ID = "net.runeduniverse.tools.runes4tools";
-	public static String ARTIFACT_ID = "runes4maven-maven-plugin";
+	public static String ARTIFACT_ID = "r4m-maven-plugin";
 	public static String PREFIX_ID = "r4m";
 
 	public interface LIFECYCLE {
@@ -20,9 +20,9 @@ public interface Runes4MavenProperties {
 			public static String EXECUTION_DEFAULT_ID = "default";
 			public static String EXECUTION_TEST_ID = "test";
 
-			public static String PARAM_SOURCE_DIR = "runes4maven.builder.main.source.dir";
-			public static String PARAM_TEST_SOURCE_DIR = "runes4maven.builder.test.source.dir";
-			public static String PARAM_TARGET_DIR = "runes4maven.builder.target.dir";
+			public static String PARAM_SOURCE_DIR = "r4m-builder.main.source.dir";
+			public static String PARAM_TEST_SOURCE_DIR = "r4m-builder.test.source.dir";
+			public static String PARAM_TARGET_DIR = "r4m-builder.target.dir";
 
 			public interface INVOKER {
 				public static String LIFECYCLE_INVOKER_HINT = "build-invoker";
@@ -37,7 +37,7 @@ public interface Runes4MavenProperties {
 
 	public interface METAINF extends MavenProperties.METAINF {
 		public interface RUNES4MAVEN {
-			public static String PATH = METAINF.PATH + "runes4maven/";
+			public static String PATH = METAINF.PATH + "r4m/";
 			public static String EXECUTIONS = PATH + "executions.xml";
 		}
 	}

720f61c75ac66e64c1a5014bc8e8b55138ea6200
VenaNocta <venanocta@gmail.com>
2025-02-21 21:44:22 +0100

    fixed NullPointer in NotDataModifierCheckFactory

--- extension/src/main/java/net/runeduniverse/tools/maven/r4m/grm/check/converter/NotDataModifierCheckFactory.java
+++ extension/src/main/java/net/runeduniverse/tools/maven/r4m/grm/check/converter/NotDataModifierCheckFactory.java
@@ -16,11 +16,8 @@
 package net.runeduniverse.tools.maven.r4m.grm.check.converter;
 
 import org.codehaus.plexus.component.annotations.Component;
-import org.codehaus.plexus.component.annotations.Requirement;
-
 import net.runeduniverse.lib.utils.conditions.api.Condition;
 import net.runeduniverse.tools.maven.r4m.grm.check.NotCheck;
-import net.runeduniverse.tools.maven.r4m.grm.check.converter.api.CheckConverter;
 import net.runeduniverse.tools.maven.r4m.grm.check.converter.api.CheckFactory;
 import net.runeduniverse.tools.maven.r4m.grm.model.DataEntry;
 import net.runeduniverse.tools.maven.r4m.grm.model.NotDataModifier;
@@ -28,9 +25,6 @@ import net.runeduniverse.tools.maven.r4m.grm.view.api.EntityView;
 
 @Component(role = CheckFactory.class, hint = NotDataModifier.CANONICAL_NAME)
 public class NotDataModifierCheckFactory extends AModifierCheckFactory {
-
-	@Requirement
-	protected CheckConverter converter;
 
 	@Override
 	public Condition<EntityView> createCheck(final DataEntry entry) {

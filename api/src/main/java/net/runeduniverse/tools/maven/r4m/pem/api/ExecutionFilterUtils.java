RunedUniverse/r4m-maven-extension

commit 6ddb121cd939f0e48d82ac0b1fbfa829785ad07d

added part 1 of pem model rework

api/src/main/java/net/runeduniverse/tools/maven/r4m/pem/api/ExecutionFilterUtils.java
@@ -39,7 +39,8 @@ public interface ExecutionFilterUtils {
 	 * @return an instance of {@link Predicate<Execution>} for filtering Executions
 	 *         by relevance utilizing {@code cnf}
 	 */
-	public static Predicate<Execution> defaultRelevanceFilter(final ExecutionArchiveSelectorConfig cnf) {
+	public static Predicate<Execution> defaultRelevanceFilter(final ExecutionRestrictionEvaluator restrictionEvaluator,
+			final ExecutionArchiveSelectorConfig cnf) {
 		return execution -> {
 			// the use of never-active flags is discouraged
 			// and included for debugging purposes
@@ -49,12 +50,11 @@ public interface ExecutionFilterUtils {
 			if (!execution.getRestrictions()
 					.isEmpty()) {
 				final Map<String, Boolean> map = new LinkedHashMap<>();
-				for (ExecutionRestriction<ExecutionArchiveSelectorConfig> restriction : execution
-						.getRestrictions(ExecutionArchiveSelectorConfig.class)) {
-					Boolean state = map.get(restriction.getHint());
-					if (state != null && state)
+				for (ExecutionRestriction restriction : execution.getRestrictions()) {
+					final Boolean state = map.get(restriction.type());
+					if (state == true)
 						continue;
-					map.put(restriction.getHint(), restriction.isActive(cnf));
+					map.put(restriction.type(), restrictionEvaluator.isActive(cnf, restriction));
 				}
 				if (map.containsValue(false))
 					return false;
@@ -78,7 +78,8 @@ public interface ExecutionFilterUtils {
 	 * @return an instance of {@link Predicate<Execution>} for filtering for active
 	 *         Executions utilizing {@code cnf}
 	 */
-	public static Predicate<Execution> defaultActiveFilter(final ExecutionArchiveSelectorConfig cnf) {
+	public static Predicate<Execution> defaultActiveFilter(final ExecutionRestrictionEvaluator restrictionEvaluator,
+			final ExecutionTriggerEvaluator triggerEvaluator, final ExecutionArchiveSelectorConfig cnf) {
 		return execution -> {
 			// the use of never-active flags is discouraged
 			// and included for debugging purposes
@@ -88,12 +89,11 @@ public interface ExecutionFilterUtils {
 			if (!execution.getRestrictions()
 					.isEmpty()) {
 				final Map<String, Boolean> map = new LinkedHashMap<>();
-				for (ExecutionRestriction<ExecutionArchiveSelectorConfig> restriction : execution
-						.getRestrictions(ExecutionArchiveSelectorConfig.class)) {
-					Boolean state = map.get(restriction.getHint());
-					if (state != null && state)
+				for (ExecutionRestriction restriction : execution.getRestrictions()) {
+					final Boolean state = map.get(restriction.type());
+					if (state == true)
 						continue;
-					map.put(restriction.getHint(), restriction.isActive(cnf));
+					map.put(restriction.type(), restrictionEvaluator.isActive(cnf, restriction));
 				}
 				if (map.containsValue(false))
 					return false;
@@ -112,10 +112,10 @@ public interface ExecutionFilterUtils {
 					.contains(execution.getId()))
 				return true;
 			// any active trigger activates the execution
-			for (ExecutionTrigger<ExecutionArchiveSelectorConfig> trigger : execution
-					.getTrigger(ExecutionArchiveSelectorConfig.class))
-				if (trigger.isActive(cnf))
+			for (ExecutionTrigger trigger : execution.getTrigger()) {
+				if (triggerEvaluator.isActive(cnf, trigger))
 					return true;
+			}
 			return false;
 		};
 	}

commit 4423e38ded7fdf1b82bfb6d28ee7833e420e424f

fixed bug introduced in 6ddb121

api/src/main/java/net/runeduniverse/tools/maven/r4m/pem/api/ExecutionFilterUtils.java
@@ -52,7 +52,7 @@ public interface ExecutionFilterUtils {
 				final Map<String, Boolean> map = new LinkedHashMap<>();
 				for (ExecutionRestriction restriction : execution.getRestrictions()) {
 					final Boolean state = map.get(restriction.type());
-					if (state == true)
+					if (state != null && state)
 						continue;
 					map.put(restriction.type(), restrictionEvaluator.isActive(cnf, restriction));
 				}
@@ -92,7 +92,7 @@ public interface ExecutionFilterUtils {
 				final Map<String, Boolean> map = new LinkedHashMap<>();
 				for (ExecutionRestriction restriction : execution.getRestrictions()) {
 					final Boolean state = map.get(restriction.type());
-					if (state == true)
+					if (state != null && state)
 						continue;
 					map.put(restriction.type(), restrictionEvaluator.isActive(cnf, restriction));
 				}